1f0828769b366c148753e566050f3dd07b60b07d
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-18 16:04:47 +0530

    added some comments and reformatted code and optimized imports

--- app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
@@ -1,13 +1,12 @@
 package sanskar.kumar.kumarsanskar.texttospeechapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
@@ -20,20 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        enterText = (EditText)findViewById(R.id.editText);
-        listenInEnglish = (Button)findViewById(R.id.button);
+        enterText = (EditText) findViewById(R.id.editText);
+        listenInEnglish = (Button) findViewById(R.id.button);
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                textToSpeech.setLanguage(Locale.ENGLISH);
-                textToSpeech.setSpeechRate(1.0f);
+                textToSpeech.setLanguage(Locale.ENGLISH);// used to set the Language
+                textToSpeech.setSpeechRate(1.0f); // the value of speech Rate is always given in Float
             }
         });
         listenInEnglish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String inputText = enterText.getText().toString();
-                textToSpeech.speak(inputText,TextToSpeech.QUEUE_FLUSH,null);
+                textToSpeech.speak(inputText, TextToSpeech.QUEUE_FLUSH, null); // QUEUE_FLUSH makes sure that no pevious data is stored
             }
         });
     }
