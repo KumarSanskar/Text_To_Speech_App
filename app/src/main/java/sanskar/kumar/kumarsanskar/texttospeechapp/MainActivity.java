a976e95c430ea4c7454057d4d68839ce1cb4a836
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-18 15:38:27 +0530

    initialized the object of textToSpeech and used setLanguage() and setSpeechRate() method

--- app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     EditText enterText;
@@ -19,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         enterText = (EditText)findViewById(R.id.editText);
         listenInEnglish = (Button)findViewById(R.id.button);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                textToSpeech.setLanguage(Locale.ENGLISH);
+                textToSpeech.setSpeechRate(1.0f);
+            }
+        });
     }
 }
