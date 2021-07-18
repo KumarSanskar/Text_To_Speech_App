2f5d1e7ee6c41ee02af13039338ba7dbabd6d743
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-18 15:19:18 +0530

    create objects for class Button and EditText

--- app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/texttospeechapp/MainActivity.java
@@ -3,8 +3,13 @@ package sanskar.kumar.kumarsanskar.texttospeechapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText enterText;
+    Button listenInEnglish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
